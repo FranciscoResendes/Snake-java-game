1fce6a647dde54bdf54aaf803648f6e06fe15eaf
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2024-09-02 16:20:36 +0100

    Game draws snake

--- GamePanel.java
+++ GamePanel.java
@@ -1,10 +1,17 @@
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel{
     static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT = 800;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    Timer timer;
+    boolean gameOver = false;
+    int bodySize = 5;
 
     GamePanel(){
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -15,5 +22,19 @@ public class GamePanel extends JPanel{
 
     public void startGame(){
 
+    }
+
+    public void draw(Graphics g){
+        for(int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++){
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.green);
+        g.fillRect(25, 25, UNIT_SIZE, UNIT_SIZE);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
     }
 }
